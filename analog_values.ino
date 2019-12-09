72ecfea550b6efcafe05833f260836f1d1cf53b3
Gedeon-m-gedus <50500636+Gedeon-m-gedus@users.noreply.github.com>
2019-12-09 15:27:39 +0000

    Add files via upload
    Arduino code to send analog values via serial communication

---
+++ analog_values.ino
@@ -0,0 +1,33 @@
+void setup() 
+{
+ pinMode(LED_BUILTIN, OUTPUT); 
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+//  if(Serial.available() > 0)
+//  {
+//    ;
+//  }
+//    char val=Serial.read();
+//
+//if( val == 's' )               
+//  {
+//
+//  digitalWrite(LED_BUILTIN, HIGH);  
+//  }
+//  if( val == 't' )               
+//  {
+//
+//  digitalWrite(LED_BUILTIN, LOW);  
+//  }
+//    
+// 
+// delay(5);
+
+int values=analogRead(A0);
+Serial.print(values);
+//Serial.write(values);
+Serial.print("$");
+}
